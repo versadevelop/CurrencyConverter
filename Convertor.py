ea8ff91c5b8ed7e0f92f200c0516beb1471f2e29
vgeorgakoudis <evangeor@teiemt.gr>
2022-09-06 10:13:44 +0300

    Fully Working Ignore Letters

--- Convertor.py
+++ Convertor.py
@@ -1,17 +1,20 @@
+from cgitb import text
+from string import digits
 from tkinter import *
 import requests
 import customtkinter
 
 options = [
-    "CAD",
-    "GBP",
-    "USD",
-    "EUR"
+    "CAD/Canada Dollar",
+    "GBP/Great Britain",
+    "USD/Dollar",
+    "EUR/Euro",
+    "BGN/Bulgaria"
 ]
 
 label = " "
-customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
 
 class CurrencyConverter:
@@ -20,14 +23,11 @@ class CurrencyConverter:
         self.currencies = self.data['rates']
 
     def convert(self, from_currency, to_currency, amount):
-        initial_amount = amount
         amount = amount / self.currencies[from_currency]
 
-            # limiting the precision to 4 decimal places
+        # 3 decimal places
         amount = round(amount * self.currencies[to_currency], 3)
-        print(amount)
         return amount
-
 
 
 class App(customtkinter.CTk):
@@ -40,57 +40,62 @@ class App(customtkinter.CTk):
         self.geometry("380x510")
         self.imported_Text = StringVar()
         self.fromtext = StringVar()
+        self.fromtext.set("EUR/Euro")
         self.totext = StringVar()
-        self.v = StringVar()
+        self.totext.set("USD/Dollar")
         inputOutputFrame(self)
         inputOutputField(self)
         dropbox(self)
         customtkinter.CTkButton(text="Convert", width=380, height=20, bd=0, bg="#0000eb",
-               cursor="hand2",
-               command=lambda: perform(self)).pack(ipady=50)
-        print(self.fromtext.get())
+                                cursor="hand2",
+                                command=lambda: execute(self)).pack(ipady=50)
+        add_label()
 
-        def perform(self):
+        def execute(self):
             global label
-            amount = float(self.imported_Text.get())
+            amount = self.imported_Text.get()
+            amount = float(''.join(c for c in amount if c in digits))
             from_curr = self.fromtext.get()
             to_curr = self.totext.get()
-
-            converted_amount= self.currency_converter.convert(from_curr,to_curr,amount)
+            converted_amount = self.currency_converter.convert(
+                from_curr[0: 3], to_curr[0: 3], amount)
             converted_amount = round(converted_amount, 2)
-            Label(converted_amount)
+            update_label(str(converted_amount), amount, from_curr, to_curr)
 
 
-
-def Label(entry):
+def add_label():
     global label
-    label = entry
-    label_1 = customtkinter.CTkLabel(text_color="black")
-    label_1.pack(pady=12, padx=10)
-    label_1.set_text(label)
-    print("Printing label ", label, "and entry ", entry)
+    label = Label(fg="blue", font="Times 14 italic")
+    label.pack(ipady=10)
 
 
+def update_label(entry, amount, fromcurre, currency):
+    global label
+    finallabel = str(amount) + " " + \
+        fromcurre[0:3] + " = " + entry + " " + currency[0:3]
+    label["text"] = str(finallabel)
 
 
 def inputOutputFrame(self):
     self.input_Output_Field_Frame = customtkinter.CTkFrame(bd=0, highlightbackground="white", highlightcolor="blue",
-                                          highlightthickness=2)
+                                                           highlightthickness=2)
     self.input_Output_Field_Frame.pack()
 
 
 def inputOutputField(self):
     self.input_Output_Field = customtkinter.CTkEntry(self.input_Output_Field_Frame,
-                                    textvariable=self.imported_Text, width=380,
-                                     bg="#ebeded", justify=LEFT)
+                                                     textvariable=self.imported_Text, width=380,
+                                                     bg="#ebeded", justify=LEFT)
     self.input_Output_Field.pack(ipady=10)
 
 
 def dropbox(self):
     self.drop = OptionMenu(self, self.fromtext, *options,)
     self.drop2 = OptionMenu(self, self.totext, *options)
-    self.drop.configure(width=50, height=6, bd=2,font=('calibri',(15)),bg='grey', highlightbackground="#5e5c5c", highlightcolor="#5e5c5c")
-    self.drop2.configure(width=50, height=6, bd=2,font=('calibri',(15)),bg='grey', highlightbackground="#5e5c5c", highlightcolor="#5e5c5c")
+    self.drop.configure(width=50, height=6, bd=2, font=(
+        'calibri', (15)), bg='grey', highlightbackground="#5e5c5c", highlightcolor="#5e5c5c")
+    self.drop2.configure(width=50, height=6, bd=2, font=(
+        'calibri', (15)), bg='grey', highlightbackground="#5e5c5c", highlightcolor="#5e5c5c")
     self.drop.pack()
     self.drop2.pack()
 
@@ -100,6 +105,3 @@ if __name__ == '__main__':
     converter = CurrencyConverter(url)
     app = App(converter)
     app.mainloop()
-
-
-
